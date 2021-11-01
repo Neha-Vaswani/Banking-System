d6678e36c3b3ce4e9f4799e00c1f3749d13973a3
Neha-Vaswani <72300522+Neha-Vaswani@users.noreply.github.com>
2021-11-01 14:20:44 +0530

    Add files via upload

---
+++ IP Assignment-2.php
@@ -0,0 +1,23 @@
+<?php
+$username = "id15696174_customer";
+$password="";
+$server ="localhost";
+$db = "id15696174_customerdetails";
+$con =
+mysqli_connect($server,$username,$password,$d
+b);
+/*if($con)
+//echo "Connection successful";
+{
+?>
+<script>
+alert('Connection Successful');
+</script>
+<?php
+}
+else
+{
+echo "No connection";
+die("no
+connection".mysqli_connect_error());//exit
+}*/
